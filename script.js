d4176c3f60591ebde7dd3c4f45e39ccb2019ee7b
brayanarmas-ux <brayan_armas@estudante.sesisenai.org.br>
2026-06-18 17:27:13 -0300

    melhores aulas

---
+++ script.js
@@ -0,0 +1,47 @@
+function guilhermeportoes(){
+
+    
+    let clts = Number(prompt("quantas clts?"))
+    let estagiarios = Number(prompt("quantos estagiarios"))
+    let pj = Number(prompt("quantos pj"))
+    let fumcionarios = estagiarios = pj
+    alert("a quantidade de funcionarios e" + funcionarios)
+    
+}
+
+
+
+function banheiro(){
+    alert("ja fez o exercicio 2?")
+      
+    
+    
+    
+}
+
+
+function peQueno(){
+    letqtdPares = Number(prompt("quantos pares?"))
+    let precoPar = Number(prompt("preco de cada"))
+    
+    let total = qtdPares *precoPar
+    alert("valor total R$" + total)
+    
+}
+
+
+
+function peba(){
+
+    
+    
+    
+    // ler o numero de vitorias
+    let vitorias = Number(prompt("quantas vitorias?"))
+    //ler o numero de empates 
+    let empates = Number(prompt("quantos empates"))
+    //calcular pontos
+    let pontos = vitorias*3 + empates
+    //mostrar resultados
+    alert("o time teu tem " + pontos)
+}
